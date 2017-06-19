477d41aea68a44058720db2d2a02425cc5234240
Dbrandy <dbrandydude@gmail.com>
2017-06-20 00:09:49 +0100

    Finished adding user input.

--- script.js
+++ script.js
@@ -4,6 +4,7 @@
 function sum(num1, num2) {
     // add parameters
     if (typeof num1 === 'string' && typeof num2 === 'string') {
+        console.log('Adding...');
         return num1 + num2;
     }
 }
