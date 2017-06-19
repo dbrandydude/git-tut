831b76cdd44fd384e29b93a3655a230f5f8b0480
Dbrandy <dbrandydude@gmail.com>
2017-06-20 00:30:27 +0100

    Finished adding script doc

--- script.js
+++ script.js
@@ -8,7 +8,8 @@
  * @return {number}      returns total sum
  */
 function sum(num1, num2) {
-    // add parameters
+    // method to implement what the class does
+    // TODO: write documentation
     if (typeof num1 === 'string' && typeof num2 === 'string') {
         console.log('Adding...');
         return num1 + num2;
