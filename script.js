8128484d0d5670e610c96c76b93dfdff1a943dd0
Dbrandy <dbrandydude@gmail.com>
2017-06-20 00:42:05 +0100

    clean code

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 /* jslint node: true */
 'use strict';
 
+/**
+ * Return the sum of two numbers
+ * @param  {number} num1 first number
+ * @param  {number} num2 second number
+ * @return {number}      returns total sum
+ */
 function sum(num1, num2) {
+    // method to implement what the class does
+    // TODO: write documentation
     if (typeof num1 === 'string' && typeof num2 === 'string') {
         console.log('Adding...');
         return num1 + num2;
